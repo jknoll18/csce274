50d26817782b16c8d8fc05d0eefbebf775f1df66
godoy159 <godoyf@email.sc.edu>
2017-09-26 16:33:53 -0400

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,31 +1,52 @@
 import serial
 import struct
 
-class interface:
+class Interface:
+    def __init__(self):
+        self.connect()
 
- def connect():
-    serial_connection = serial.Serial('/dev/ttyUSB0', 115200)
-    
-    return serial_connection
- def read(serial conn):
-   conn = serial.Serial('/dev/ttyUSB0', 115200) 
-   string var = con.read(1)
-   int val = struct.unpack('B',var)
-   if val = 10:
-     print('safe')
-   else 
-     print('passive')
- def command(int num, serial conn):
-   if num = 128:
-     def start():
-       
-   elif num = 173:    
-     def stop():
-   elif num = 7:
-     def reset():
-   elif num = 128:
-     def passive():
-   elif num = 131:
-     def safe():
-   elif num = 137:
-     def drive():
+    def connect(self):
+        self.serial_connection = serial.Serial('/dev/ttyUSB0', 115200)
+
+    def write_raw(raw_command):
+        self.serial_connection.write(raw_command)
+
+    def read_raw(num_bytes):
+        raw_data = self.serial_connection.read(num_bytes)
+        return raw_data
+
+
+    def read_sensor(self, sensor_packet_id):
+        """
+        Args:
+            sensor_packet_id (int): ID of the sensor packet.
+        """
+        # write
+        raw_command = chr(142) + chr(sensor_packet_id)
+        self.serial_connection.write(raw_command)
+        
+        # read raw
+        
+        var = self.serial_connection.read(1)
+        
+        # Processing
+        val = struct.unpack('B',var)
+        if val = 10:
+            print('safe')
+        else:
+            print('passive')
+
+    def command(self, num):
+        if num == 128:
+            raw_command = chr(128)
+            self.write_raw(raw_command)
+        elif num = 173:
+            def stop():
+        elif num = 7:
+            def reset():
+        elif num = 128:
+            def passive():
+        elif num = 131:
+            def safe():
+        elif num = 137:
+            def drive():
