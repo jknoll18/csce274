d415b47d334c7b025a9f1b027f08f40c7a20cafd
godoy159 <godoyf@email.sc.edu>
2017-10-06 15:36:43 -0400

    Add files via upload

---
+++ Project_1/reading.py
@@ -0,0 +1,10 @@
+
+"""
+from interface import Interface
+def checkSensor(on):
+  inter = Interface()
+  while(on == True):
+    inter.read_sensor(18) ##need something 
+    ##to read the button press and execute penta, use threading?
+
+"""
