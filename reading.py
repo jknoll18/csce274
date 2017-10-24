575cb15ef70304605ee5ae1cef6b0294200b9dcc
godoy159 <godoyf@email.sc.edu>
2017-10-23 22:20:27 -0400

    edit

--- reading.py
+++ reading.py
@@ -2,7 +2,7 @@ from interface import Interface
 from threading import Timer
 import random
 import time
-class Reading: 
+class Reading:
   def __init__(self):
     press = False
   def isActive(self):
@@ -17,15 +17,16 @@ class Reading:
           count += 1
         else:
           Interface().stop
-      if sw == 0:
-        if state % 2 == 0:
-          Interface().penta()
-          count += 1
-        else:
-          Interface().stop
+            if sw == 0:
+              if state % 2 == 0:
+                Interface().penta()
+                count += 1
+              else:
+                Interface().stop
   def isActive2(self):
     a = 1
-    while(a == 1): 
+    ##Interface().full()
+    while(a == 1):
       state = 0
       while(state == 0):
         sw = self.readall()
@@ -37,42 +38,26 @@ class Reading:
         sw7 = Interface().read_sensor(18)
         if( sw7 == 1):
           Interface().stop()
-          ##seven = Interface().read_sensor(7)
-          ##s3 = seven[1]
-          ##s4 = seven[0]
-          ##if( s3 == "1" or s4 == "1"):
-          ##Interface().play(0)
           state = 0
         else:
           seven = Interface().read_sensor(7)
           eight = self.readall()
-          if(seven[0] == "1" or seven[1] == "1"):
-		    ##stop
+          if(seven[0] == "1" or  seven[1] == "1"):
+            ## Interface().song()
+            Interface().play(0)
+            time.sleep(5)
             Interface().stop()
-			Interface().play(0)
-		    ##set to stopped
+            ##set to stopped
             state = 0
-          elif(seven[2] == "1" or seven[3] == "1" and eight == 1):  ##rotate only
-            if(seven[3] == "1"):
-			  offset = random.uniform(-0.5,0.5)
-			  offset2 = float(3 + offset)
-              timer0 = Timer(0.0,Interface().drive, args = (100,1))
-              timer0.start()
-              time.sleep(offset2)
-              timer0.cancel()
-			  Inteface().stop()
-			  state = 0
-              ##rotate cw 180 plus random angle then forward
-            else:
-			  offset = random.uniform(-0.5,0.5)
-			  offset2 = float(3 + offset)
-              timer0 = Timer(0.0,Interface().drive, args = (100,-1))
-              timer0.start()
-              time.sleep(3)
-              timer0.cancel()
-			  Inteface().stop()
-			  state = 0
-              ## rotate ccw 180 plus random angle then forward		
+          elif(eight == 1):  ##rotate only
+            offset = random.uniform(-0.5,0.5)
+            offset2 = float(3 + offset)
+            timer0 = Timer(0.0,Interface().drive, args = (100,1))
+            timer0.start()
+            time.sleep(offset2)
+            timer0.cancel()
+            ##Interface().stop()
+            ##state = 0
           elif(seven[3] == "1"): #bumper right cond
             ##rotate cw 180 plus random angle then move forward
             offset = random.uniform(-0.5,0.5)
@@ -83,7 +68,7 @@ class Reading:
             timer0.cancel()
           elif(seven[2] == "1"): ## bumper left cond
             ## rotate ccw 180 plus rand angle then forward
-			offset = random.uniform(-0.5,0.5)
+            offset = random.uniform(-0.5,0.5)
             offset2 = float(3 + offset)
             timer0 = Timer(0.0,Interface().drive, args = (100,-1))
             timer0.start()
