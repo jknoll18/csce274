5b049b711e626ca667a6e33641255aa5d0157653
godoy159 <godoyf@email.sc.edu>
2017-11-03 21:13:09 -0400

    comm

--- interface.py
+++ interface.py
@@ -69,34 +69,38 @@ class Interface:
       if (bits[1] == '1'):
         self.serial_connection(chr(142)+chr(50))
         rb = self.serial_connection.read(2)
-        struct.unpack('>2B',rb )[0]
+        RFB = struct.unpack('>2B',rb )[0]
         print 'Right Front Bumper Active'
+        return RFB
         ## TODO Check Packet ID 50 to see the signal strength
       if (bits[2] == '1'):
         self.serial_connection(chr(142)+chr(49))
         rb = self.serial_connection.read(2)
-        struct.unpack('>2B',rb )[0]
+        RCB = struct.unpack('>2B',rb )[0]
         print 'Right Center Bumper Active'
+        return RCB
         ## TODO Check Packet ID 49 to see the signal strength
       if (bits[3] == '1'):
         self.serial_connection(chr(142)+chr(48))
         rb = self.serial_connection.read(2)
-        struct.unpack('>2B',rb )[0]
+        LCB = struct.unpack('>2B',rb )[0]
         print 'Left Center Bumper Active'
+        return LCB
         ## TODO Check Packet ID 48 to see the signal strength
       if (bits[4] == '1'):
         self.serial_connection(chr(142)+chr(47))
         rb = self.serial_connection.read(2)
-        struct.unpack('>2B',rb )[0]
+        LFB = struct.unpack('>2B',rb )[0]
         print 'Left Front Bumper Active'
+        return LFB
         ## TODO Check Packet ID 47 to see the signal strength
       if (bits[5] == '1'):
         self.serial_connection(chr(142)+chr(46))
         rb = self.serial_connection.read(2)
-        struct.unpack('>2B',rb )[0]
-        print 'Left Bumper Active'
+        LCB = struct.unpack('>2B',rb )[0]
+        print 'Left Center Bumper Active'
+        return LCB
         ## TODO Check Packet ID 46 to see the signal strength
-      return bits
     ##look up how to convert a string hex into a signed int.
     ##it both distance and angle we need to find the raw encoder count 
     ##using this formula, it's sensor packet 43, 44
